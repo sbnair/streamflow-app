d2f44d273b7575c51c1810a66c9e42d4bb1d290e
imprfekt <5047969+imprfekt@users.noreply.github.com>
2021-06-07 02:12:53 +0200

    remove solana program from this repository

--- program/cli/local_validator_tmux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-rm -rf test-ledger
-tmux new-session -d "solana-test-validator --limit-ledger-size 999999"
-sleep 1
-tmux split-window -v "solana logs --commitment processed"
-tmux a
