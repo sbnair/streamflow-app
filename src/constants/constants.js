897c32b2614e3ae40d6afd2bba9dce6ff93ff7aa
imprfekt <5047969+imprfekt@users.noreply.github.com>
2021-06-08 12:49:38 +0200

    add constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,9 @@
 export const SOLLET_URL = 'https://www.sollet.io';
 export const AIRDROP_AMOUNT = 10; //10 is the cap on the devnet
 
+export const TX_FINALITY_CONFIRMED = "confirmed";
+export const TX_FINALITY_FINALIZED = "finalized";
+
 export const INSTRUCTION_CREATE_STREAM = 0;
 export const INSTRUCTION_WITHDRAW_STREAM = 1;
 export const INSTRUCTION_CANCEL_STREAM = 2;
