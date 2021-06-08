30edd427b0b06aab8fa5953d511005a995ecd786
imprfekt <5047969+imprfekt@users.noreply.github.com>
2021-06-08 13:43:03 +0200

    add receiver to the cancel

--- src/Actions/cancelStream.js
+++ src/Actions/cancelStream.js
@@ -7,17 +7,21 @@ import sendTransaction from "./sendTransaction";
 import Wallet from "@project-serum/sol-wallet-adapter";
 
 export default async function _cancelStream(account_id: string, data: StreamData, connection: Connection, wallet: Wallet, network: string) {
-    const {sender} = data;
-    const instruction = getCancelStreamInstruction(account_id, sender)
+    const {sender, receiver} = data;
+    const instruction = getCancelStreamInstruction(account_id, sender, receiver)
     const tx = new Transaction().add(instruction);
     return await sendTransaction(INSTRUCTION_CANCEL_STREAM, tx, connection, wallet, network)
 }
 
-function getCancelStreamInstruction(account_id, sender) {
+function getCancelStreamInstruction(account_id, sender, receiver) {
     return new TransactionInstruction({
         keys: [{
             pubkey: new PublicKey(sender),
             isSigner: true,
+            isWritable: true
+        }, {
+            pubkey: new PublicKey(receiver),
+            isSigner: false,
             isWritable: true
         }, {
             pubkey: new PublicKey(account_id),
