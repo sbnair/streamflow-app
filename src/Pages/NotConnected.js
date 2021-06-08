6554ed0a1957d72dc40a8fa6da5038906ab9dd5d
imprfekt <5047969+imprfekt@users.noreply.github.com>
2021-06-08 02:18:38 +0200

    Update video

--- src/Pages/NotConnected.js
+++ src/Pages/NotConnected.js
@@ -1,10 +1,9 @@
 import ButtonPrimary from "../Components/ButtonPrimary";
 
 export default function NotConnected(props: { action: () => * }) {
-    return <div className="max-w-lg mx-auto">
-        <iframe width="100%" height={270} src="https://www.youtube.com/embed/KMU0tzLwhbE"
-                title="YouTube video player" frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    return <div className="max-w-md mx-auto">
+        <iframe width="100%" height={270} src="https://www.youtube-nocookie.com/embed/7hrv7HDK3oE"
+                title="StreamFlow Finance - Solana Season Hackathon Demo" frameBorder="0"
                 allowFullScreen>&nbsp;</iframe>
         <ButtonPrimary text="Connect" className="font-bold text-2xl my-5"
                        action={props.action}/>
