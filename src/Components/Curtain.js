82948c48feb61222979d002a5b4e26b6f449a017
imprfekt <5047969+imprfekt@users.noreply.github.com>
2021-06-08 12:49:26 +0200

    update curtain color

--- src/Components/Curtain.js
+++ src/Components/Curtain.js
@@ -1,6 +1,6 @@
 export default function Curtain(props: { visible: boolean }) {
     return (
-        <div className={`fixed top-0 bottom-0 left-0 right-0 bg-white opacity-90 z-10 ${props.visible ? "block" : "hidden"}`}>
+        <div className={`fixed top-0 bottom-0 left-0 right-0 bg-gray-900 opacity-90 z-10 ${props.visible ? "block" : "hidden"}`}>
             <div className="loader"> </div>
         </div>
     )
